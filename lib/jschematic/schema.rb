0c85df46502b99b4835c7ef17cf95e459f9219f9
Mike Sassak <msassak@gmail.com>
2011-01-10 22:35:52 -0600

    Extract schema from validator

---
+++ lib/jschematic/schema.rb
@@ -0,0 +1,55 @@
+require 'jschematic/attributes'
+
+module Jschematic
+  class Schema
+    attr_reader :schema, :instance
+
+    def initialize(schema, instance)
+      @schema, @instance = schema, instance
+    end
+
+    def valid?
+      [
+        type_valid?(schema["type"], instance),
+        minimum_valid?(*schema.values_at("minimum", "exclusiveMinimum"), instance),
+        maximum_valid?(*schema.values_at("maximum", "exclusiveMaximum"), instance),
+        properties_valid?(schema["properties"], instance)
+      ].all? { |res| res == true }
+    end
+
+    def type_valid?(type, json)
+      Attributes::Type.new(type, json).valid?
+    end
+
+    def minimum_valid?(min, exclusive, num)
+      return true unless min
+      return true unless (num.kind_of?(Integer) || num.kind_of?(Float))
+
+      if exclusive
+        num > min
+      else
+        num >= min
+      end
+    end
+
+    def maximum_valid?(max, exclusive, num)
+      return true unless max
+      return true unless (num.kind_of?(Integer) || num.kind_of?(Float))
+
+      if exclusive
+        num < max
+      else
+        num <= max
+      end
+    end
+
+    def properties_valid?(schema, json)
+      return true unless schema
+
+      schema.all? do |property, schema|
+        type_valid?(schema["type"], json[property]) &&
+          maximum_valid?(*schema.values_at("maximum", "exclusiveMaximum"), json[property])
+      end
+    end
+  end
+end
--- lib/jschematic/validator.rb
+++ lib/jschematic/validator.rb
@@ -1,4 +1,4 @@
-require 'jschematic/attributes/type'
+require 'jschematic/schema'
 
 module Jschematic
   class Validator
@@ -9,47 +9,7 @@ module Jschematic
     end
 
     def valid?
-      [
-        type_valid?(schema["type"], json),
-        minimum_valid?(*schema.values_at("minimum", "exclusiveMinimum"), json),
-        maximum_valid?(*schema.values_at("maximum", "exclusiveMaximum"), json),
-        properties_valid?(schema["properties"], json)
-      ].all? { |res| res == true }
-    end
-
-    def type_valid?(type, json)
-      Attributes::Type.new(type, json).valid?
-    end
-
-    def minimum_valid?(min, exclusive, num)
-      return true unless min
-      return true unless (num.kind_of?(Integer) || num.kind_of?(Float))
-
-      if exclusive
-        num > min
-      else
-        num >= min
-      end
-    end
-
-    def maximum_valid?(max, exclusive, num)
-      return true unless max
-      return true unless (num.kind_of?(Integer) || num.kind_of?(Float))
-
-      if exclusive
-        num < max
-      else
-        num <= max
-      end
-    end
-
-    def properties_valid?(schema, json)
-      return true unless schema
-
-      schema.all? do |property, schema|
-        type_valid?(schema["type"], json[property]) &&
-          maximum_valid?(*schema.values_at("maximum", "exclusiveMaximum"), json[property])
-      end
+      Schema.new(schema, json).valid?
     end
   end
 end
